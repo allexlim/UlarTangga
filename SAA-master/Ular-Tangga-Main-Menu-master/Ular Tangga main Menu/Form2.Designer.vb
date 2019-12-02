aafd9a617d2e6db5c5142bed73ceb8438d367954
allexlim <57930167+allexlim@users.noreply.github.com>
2019-12-02 22:11:38 +0700

    Add files via upload

---
+++ SAA-master/Ular-Tangga-Main-Menu-master/Ular Tangga main Menu/Form2.Designer.vb
@@ -0,0 +1,152 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FormPlay
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.BtnDadu = New System.Windows.Forms.Button()
+        Me.LbDadu = New System.Windows.Forms.Label()
+        Me.BtnCheat = New System.Windows.Forms.Button()
+        Me.TbCheat = New System.Windows.Forms.TextBox()
+        Me.Tm1 = New System.Windows.Forms.Timer(Me.components)
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.ListBox2 = New System.Windows.Forms.ListBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Tmrandom1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Tmrandom2 = New System.Windows.Forms.Timer(Me.components)
+        Me.SuspendLayout()
+        '
+        'BtnDadu
+        '
+        Me.BtnDadu.Location = New System.Drawing.Point(579, 99)
+        Me.BtnDadu.Name = "BtnDadu"
+        Me.BtnDadu.Size = New System.Drawing.Size(75, 23)
+        Me.BtnDadu.TabIndex = 0
+        Me.BtnDadu.Text = "Dadu"
+        Me.BtnDadu.UseVisualStyleBackColor = True
+        '
+        'LbDadu
+        '
+        Me.LbDadu.AutoSize = True
+        Me.LbDadu.Font = New System.Drawing.Font("Niagara Engraved", 48.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Strikeout), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LbDadu.Location = New System.Drawing.Point(576, 9)
+        Me.LbDadu.Name = "LbDadu"
+        Me.LbDadu.Size = New System.Drawing.Size(46, 68)
+        Me.LbDadu.TabIndex = 1
+        Me.LbDadu.Text = "_"
+        '
+        'BtnCheat
+        '
+        Me.BtnCheat.Location = New System.Drawing.Point(579, 193)
+        Me.BtnCheat.Name = "BtnCheat"
+        Me.BtnCheat.Size = New System.Drawing.Size(75, 23)
+        Me.BtnCheat.TabIndex = 2
+        Me.BtnCheat.Text = "Cheat"
+        Me.BtnCheat.UseVisualStyleBackColor = True
+        '
+        'TbCheat
+        '
+        Me.TbCheat.Location = New System.Drawing.Point(568, 147)
+        Me.TbCheat.Name = "TbCheat"
+        Me.TbCheat.Size = New System.Drawing.Size(100, 20)
+        Me.TbCheat.TabIndex = 3
+        '
+        'Tm1
+        '
+        Me.Tm1.Interval = 500
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.Location = New System.Drawing.Point(485, 264)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(120, 95)
+        Me.ListBox1.TabIndex = 4
+        '
+        'ListBox2
+        '
+        Me.ListBox2.FormattingEnabled = True
+        Me.ListBox2.Location = New System.Drawing.Point(634, 264)
+        Me.ListBox2.Name = "ListBox2"
+        Me.ListBox2.Size = New System.Drawing.Size(120, 95)
+        Me.ListBox2.TabIndex = 5
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(565, 391)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(39, 13)
+        Me.Label1.TabIndex = 6
+        Me.Label1.Text = "Label1"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(628, 391)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(13, 13)
+        Me.Label2.TabIndex = 7
+        Me.Label2.Text = "0"
+        '
+        'Tmrandom1
+        '
+        Me.Tmrandom1.Interval = 50
+        '
+        'Tmrandom2
+        '
+        Me.Tmrandom2.Enabled = True
+        Me.Tmrandom2.Interval = 50
+        '
+        'FormPlay
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.ListBox2)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.TbCheat)
+        Me.Controls.Add(Me.BtnCheat)
+        Me.Controls.Add(Me.LbDadu)
+        Me.Controls.Add(Me.BtnDadu)
+        Me.Name = "FormPlay"
+        Me.Text = "Form2"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents BtnDadu As Button
+    Friend WithEvents LbDadu As Label
+    Friend WithEvents BtnCheat As Button
+    Friend WithEvents TbCheat As TextBox
+    Friend WithEvents Tm1 As Timer
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents ListBox2 As ListBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Tmrandom1 As Timer
+    Friend WithEvents Tmrandom2 As Timer
+End Class
---
+++ SAA-master/Ular-Tangga-Main-Menu-master/Ular Tangga main Menu/Form2.vb
@@ -0,0 +1,214 @@
+﻿Public Class FormPlay
+    Dim X(10, 10) As Button
+    Dim Y(1) As Integer
+    Dim Z(3) As Button
+    Dim P(5, 1) As Integer
+    Dim A As Integer
+    Dim B As Integer = 0
+    Dim C, R, T As Integer
+    Dim D As Integer = 1
+    Dim Rndm As New Random
+    Dim F As Integer = 0
+    Dim G(5) As Integer
+    Dim H As Integer = 0
+    Dim Q(5) As Integer
+    Dim S(3) As Integer
+    Dim angka As Integer = 30
+    Dim simpan As Integer
+    Dim hello As Integer = 0
+    Private Sub FormPlay_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        If Main_Menu.RB_P2.Checked = True Then
+            R = 2
+        ElseIf Main_Menu.RB_P3.Checked = True Then
+            R = 3
+        ElseIf Main_Menu.RB_P4.Checked = True Then
+            R = 4
+        End If
+        If Main_Menu.RB_Easy.Checked = True Or Main_Menu.RB_Med.Checked = True Then
+            T = 8
+        ElseIf Main_Menu.RB_Har.Checked = True Then
+            T = 9
+        ElseIf Main_Menu.RB_Ext.Checked = True Or Main_Menu.RB_Idi.Checked = True Then
+            T = 10
+        End If
+        For i = 0 To 5
+            P(i, 0) = Rndm.Next(20, T * T - 1)
+            For j = 0 To i - 1
+                While P(i, 0) = P(j, 0)
+                    P(i, 0) = Rndm.Next(20, T * T - 1)
+                End While
+            Next
+            P(i, 1) = Rndm.Next(2, T * T - 1)
+            While P(i, 1) >= P(i, 0) - 10
+                P(i, 1) = Rndm.Next(2, T * T - 1)
+            End While
+            ListBox1.Items.Add(P(i, 0))
+            ListBox2.Items.Add(P(i, 1))
+        Next
+        For i = 0 To R - 1
+            Z(i) = New Button
+            Z(i).Height = 20
+            Z(i).Width = 20
+            If i = 0 Then
+                Z(i).BackColor = Color.Red
+                Z(i).Left = 0
+                Z(i).Top = 40 * (T - 1)
+            ElseIf i = 1 Then
+                Z(i).BackColor = Color.Blue
+                Z(i).Left = 20
+                Z(i).Top = 40 * (T - 1)
+            ElseIf i = 2 Then
+                Z(i).BackColor = Color.Black
+                Z(i).Left = 0
+                Z(i).Top = 40 * (T - 0.5)
+            ElseIf i = 3 Then
+                Z(i).BackColor = Color.Brown
+                Z(i).Left = 20
+                Z(i).Top = 40 * (T - 0.5)
+            End If
+            Me.Controls.Add(Z(i))
+        Next
+        For i = 0 To T - 1
+            For j = 0 To T - 1
+                X(i, j) = New Button
+                X(i, j).Height = 40
+                X(i, j).Width = 40
+                X(i, j).Top = 40 * T - (i + 1) * 40
+                If i Mod 2 = 0 Then
+                    X(i, j).Left = 40 * j
+                Else
+                    X(i, j).Left = 40 * T - (j + 1) * 40
+                End If
+                B = B + 1
+                X(i, j).Text = B
+                X(i, j).Name = "X(" & B & ")"
+                If B = P(0, 0) Or B = P(1, 0) Or B = P(2, 0) Or B = P(3, 0) Or B = P(4, 0) Or B = P(5, 0) Then
+                    X(i, j).BackColor = Color.Red
+                ElseIf B = P(0, 1) Or B = P(1, 1) Or B = P(2, 1) Or B = P(3, 1) Or B = P(4, 1) Or B = P(5, 1) Then
+                    X(i, j).BackColor = Color.Blue
+                End If
+                Me.Controls.Add(X(i, j))
+            Next
+        Next
+        For i = 0 To 5
+            For j = 0 To T - 1
+                For k = 0 To T - 1
+                    If X(j, k).Text = P(i, 1) Then
+                        G(i) = X(j, k).Left
+                        Q(i) = X(j, k).Top
+                    End If
+                Next
+            Next
+        Next
+        For i = 0 To R - 1
+            S(i) = 1
+        Next
+    End Sub
+
+    Private Sub BtnDadu_Click(sender As Object, e As EventArgs) Handles BtnDadu.Click
+        F = CInt(Label2.Text)
+        Tmrandom1.Enabled = True
+        Tmrandom2.Enabled = True
+        LbDadu.Font = New Font(LbDadu.Font, FontStyle.Strikeout)
+        C = 0
+        BtnDadu.Enabled = False
+        If F = 0 Then
+            H = 0
+            F = 1
+        ElseIf F = 1 Then
+            H = 1
+            F = 2
+        ElseIf F = 2 Then
+            H = 2
+            F = 3
+        ElseIf F = 3 Then
+            H = 3
+            F = 0
+        End If
+        If F = R Then
+            H = 0
+        End If
+        If A = 6 Then
+            F = H
+        End If
+        Label1.Text = H
+        Label2.Text = F
+        AddHandler Tm1.Tick, AddressOf Main
+    End Sub
+
+    Private Sub Tmrandom1_Tick(sender As Object, e As EventArgs) Handles Tmrandom1.Tick
+        Randomize()
+        If angka > 0 Then
+            A = Rnd() * 5 + 1
+            LbDadu.Text = A
+            angka -= 1
+        Else
+            Tmrandom1.Enabled = False
+            hello = 1
+        End If
+    End Sub
+
+    Private Sub Tmrandom2_Tick(sender As Object, e As EventArgs) Handles Tmrandom2.Tick
+        Randomize()
+        If hello = 1 Then
+            Tmrandom2.Enabled = False
+            A = Rnd() * 5 + 1
+            LbDadu.Text = A
+            LbDadu.Font = New Font(LbDadu.Font, FontStyle.Underline)
+            angka = 30
+            hello = 0
+            Tm1.Enabled = True
+        End If
+    End Sub
+
+    Sub Main(ByVal sender As Object, ByVal e As EventArgs)
+        H = CInt(Label1.Text)
+        If C < A Then
+            If S(H) Mod T = 0 Then
+                Z(H).Top = Z(H).Top - 40
+            ElseIf S(H) > T And S(H) < T * 2 Or S(H) > T * 3 And S(H) < T * 4 Or S(H) > T * 5 And S(H) < T * 6 Or S(H) > T * 7 And S(H) < T * 8 Or S(H) > T * 9 And S(H) < T * 10 Then
+                Z(H).Left = Z(H).Left - 40
+            Else
+                Z(H).Left = Z(H).Left + 40
+            End If
+            S(H) = S(H) + 1
+            C = C + 1
+            'MsgBox(Z(1).Left.ToString)
+        Else
+            For i = 0 To 5
+                If D = P(i, 0) Then
+                    Z(H).Location = New Point(G(i) + 20, Q(i))
+                    D = P(i, 1)
+                End If
+            Next
+            Tm1.Enabled = False
+            BtnDadu.Enabled = True
+        End If
+    End Sub
+    Private Sub BtnCheat_Click(sender As Object, e As EventArgs) Handles BtnCheat.Click
+        Tm1.Enabled = True
+        A = TbCheat.Text
+        C = 0
+        If F = 0 Then
+            H = 0
+            F = 1
+        ElseIf F = 1 Then
+            H = 1
+            F = 2
+        ElseIf F = 2 Then
+            H = 2
+            F = 3
+        ElseIf F = 3 Then
+            H = 3
+            F = 0
+        End If
+        If F = R Then
+            H = 0
+        End If
+        If A = 6 Then
+            F = H
+        End If
+        Label1.Text = H
+        AddHandler Tm1.Tick, AddressOf Main
+    End Sub
+End Class
